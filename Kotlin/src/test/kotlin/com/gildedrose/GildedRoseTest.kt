3e54fec6cc1014ed7fc8b42818221fafdfda0242
Daniel <daniel.zappold@gmx.de>
2020-10-27 09:54:50 +0100

    "mob next [ci-skip]"

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -3,8 +3,10 @@ package com.gildedrose
 import org.approvaltests.Approvals.verifyAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 internal class GildedRoseTest {
+    private val random = Random(100)
 
     @Test
     fun foo() {
@@ -16,14 +18,29 @@ internal class GildedRoseTest {
 
     @Test
     fun goldenMaster() {
-        val items = genarateItems()
+        val items = genarateItems(10000)
         val app = GildedRose(items)
         app.updateQuality()
         verifyAll("items", items)
     }
 
-    private fun genarateItems(): Array<Item> {
-        return arrayOf(Item("foo", 0, 0))
+    private fun genarateItems(count: Int): Array<Item> {
+        return (0..count).map { generateItem() }.toTypedArray()
+    }
+
+    private fun generateItem() = Item(generateName(), generateSellIn(), generateQuality())
+
+    private fun generateQuality(): Int {
+        return random.nextInt(-500, 500)
+    }
+
+    private fun generateName(): String {
+        val possibleNames = listOf("foo", "Aged Brie")
+        return possibleNames[random.nextInt(0, possibleNames.size)]
+    }
+
+    private fun generateSellIn(): Int {
+        return random.nextInt(-500, 500)
     }
 
 }

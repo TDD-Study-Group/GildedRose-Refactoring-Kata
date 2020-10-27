9cf458924ed8836b038bf4293898e2362f4daa1c
ULLXM <me@you.de>
2020-10-27 11:10:19 +0100

    "mob next [ci-skip]"

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,60 +4,47 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            fun reduceQuality() {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            }
+            updateItem(item)
+        }
+    }
 
-            fun increaseQuality() {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
-            }
+    private fun updateItem(item: Item) {
+        fun reduceQuality() {
+            if (item.quality > 0) item.quality = item.quality - 1
+        }
 
-            fun reduceSellIn() {
-                item.sellIn = item.sellIn - 1
-            }
+        fun increaseQuality() {
+            if (item.quality < 50) item.quality = item.quality + 1
+        }
 
+        fun reduceSellIn() {
+            item.sellIn = item.sellIn - 1
+        }
 
-            if (item.name == "Aged Brie") {
-                increaseQuality()
-                reduceSellIn()
-                if (item.sellIn < 0) {
-                    increaseQuality()
-                }
-            } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-
-                    if (item.sellIn < 11) {
-                        increaseQuality()
-                    }
-
-                    if (item.sellIn < 6) {
-                        increaseQuality()
-                    }
-                }
-                reduceSellIn()
-                if (item.sellIn < 0) {
-                    item.quality = 0
-                }
-            } else if (item.name == "Sulfuras, Hand of Ragnaros") {
-            } else if (item.name.startsWith("Conjured")) {
+        if (item.name == "Aged Brie") {
+            increaseQuality()
+            reduceSellIn()
+            if (item.sellIn < 0) increaseQuality()
+        } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            increaseQuality()
+            if (item.sellIn < 11) increaseQuality()
+            if (item.sellIn < 6) increaseQuality()
+            reduceSellIn()
+            if (item.sellIn < 0) item.quality = 0
+        } else if (item.name == "Sulfuras, Hand of Ragnaros") {
+        } else if (item.name.startsWith("Conjured")) {
+            reduceQuality()
+            reduceQuality()
+            reduceSellIn()
+            if (item.sellIn < 0) {
                 reduceQuality()
                 reduceQuality()
-                reduceSellIn()
-                if (item.sellIn < 0) {
-                    reduceQuality()
-                    reduceQuality()
-                }
-            } else {
+            }
+        } else {
+            reduceQuality()
+            reduceSellIn()
+            if (item.sellIn < 0) {
                 reduceQuality()
-                reduceSellIn()
-                if (item.sellIn < 0) {
-                    reduceQuality()
-                }
             }
         }
     }
